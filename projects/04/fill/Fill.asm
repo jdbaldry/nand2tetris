e024e50fe196dbf655ea63d5f306abe985f54b1c
Jack Baldry <jack.baldry@grafana.com>
2021-07-09 11:22:37 +0100

    feat: Implement Fill program
    Signed-off-by: Jack Baldry <jack.baldry@grafana.com>

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -6,9 +6,76 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+  @i
+  M=0
+
+  @8191
+  D=A
+  @n
+  M=D
+
+(LOOP)
+  @i
+  D=M
+  @RESET
+  D;JLT
+  @n
+  D=D-M
+  @STOP
+  D;JGT
+
+  @KBD
+  D=M
+  @UNPAINT
+  D;JEQ
+  @PAINT
+  0;JMP
+
+(RESET)
+  @i
+  M=0
+  @LOOP
+  0;JMP
+
+(STOP)
+  @n
+  D=M
+  @i
+  M=D
+  @LOOP
+  0;JMP
+
+(PAINT)
+  @i
+  D=M
+  @SCREEN
+  A=D+A
+  M=-1
+
+  @i
+  M=M+1
+
+  @LOOP
+  0;JMP
+
+(UNPAINT)
+  @i
+  D=M
+  @SCREEN
+  A=D+A
+  M=0
+
+  @i
+  M=M-1
+
+  @LOOP
+  0;JMP
+
+(END)
+  @END
+  0;JMP
